4f5838cf6732c459cfa60278c0e6ed2e9683998f
ValoUK <valentin.aj.roy@gmail.com>
2015-07-06 01:00:36 +0100

    add FileParser

---
+++ FileParser.cpp
@@ -0,0 +1,31 @@
+#include "FileParser.h"
+#include <iostream> 
+#include <fstream> 
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+
+std::map<std::string, std::string> &FileParser::readFile(std::string &filePath)
+{
+	std::map<std::string, std::string> map;
+	std::ifstream inputFile(filePath.c_str());
+	std::string line,key,value;
+	if (inputFile.is_open())
+	{
+		while (std::getline(inputFile, line))
+		{
+			if (boost::algorithm::contains(line, "#"))
+			{
+				key = line;
+				boost::erase_all(key, "#");
+				boost::trim(key);
+			}
+			else
+			{
+				value = line;
+				boost::trim(value);
+				map[key] = value;
+			}
+		}
+	} 
+	return map;
+}
---
+++ FileParser.h
@@ -0,0 +1,20 @@
+#ifndef FILEPARSER_H
+#define FILEPARSER_H
+
+#include <map> //std::map
+#include <iostream> 
+#include <string> //std::string
+#include <vector> //std::vector
+#include <fstream> //std::ifstream
+#include <sstream> // std::istringstream
+
+class FileParser
+{
+public:
+	std::map<std::string, std::string> &readFile(std::string &filePath);
+private:
+	//std::ifstream  _inputFile;
+	std::string _filePath;
+};
+
+#endif
